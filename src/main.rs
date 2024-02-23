0abe1f89ad432655c4e65f86976b6bd8940a2999
Faizan <faizan.sarwer@gmail.com>
2024-02-23 20:07:17 +0530

    telemetry added for loggin throughout the app

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("Zero2Prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection = PgPool::connect(&configuration.database.connection_string())
+    let connection = PgPool::connect(&configuration.database.connection_string().expose_secret())
         .await
         .expect("Failed to connection to database");
     // Port is coming from our settings!
