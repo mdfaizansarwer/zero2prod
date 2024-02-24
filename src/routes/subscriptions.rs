95f1012f72280c550bf878db82726baf8606869e
faizan sarwer <faizan.sarwer@gmail.com>
2024-02-24 15:06:44 +0530

    Dockerization of the app (#4)
    * telemetry added for loggin throughout the app
    
    * workflow file update for the branch
    
    * lint fixed
    
    * dockerized the app
    
    * pipeline branch updated
    
    * merge conflict resolved

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse, Responder};
 use chrono::Utc;
 use sqlx::PgPool;
+use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -45,8 +46,19 @@ pub async fn subscriptions(
     form: web::Form<FormData>,
     db_pool: web::Data<PgPool>,
 ) -> impl Responder {
+    if !is_valid_name(&form.name) {
+        return HttpResponse::BadRequest().finish();
+    }
     match insert_subsriber(&db_pool, &form).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
+}
+
+pub fn is_valid_name(s: &str) -> bool {
+    let is_empty_or_whitespace = s.trim().is_empty();
+
+    let is_too_long = s.graphemes(true).count() > 256;
+
+    !(is_empty_or_whitespace || is_too_long)
 }
